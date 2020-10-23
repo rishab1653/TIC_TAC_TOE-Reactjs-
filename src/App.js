565d915b87e5568cbd2537a357e45fa64a7d152a
rishab1653 <rishab959290@gmail.com>
2020-10-23 22:45:16 +0530

    Added REact tostify and react strap imports

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Icon from './Components/Icon'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import {Card,CardBody,Container,Button,Col,Row} from 'reactstrap'
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
