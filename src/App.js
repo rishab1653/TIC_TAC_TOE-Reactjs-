4cfa1859f38bcd2451a8c72e673b5aeed0df583f
rishab1653 <rishab959290@gmail.com>
2020-10-23 18:30:11 +0530

    Sending icon name as a probs

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
        
-        <p>
+        <p> 
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <Icon/>
