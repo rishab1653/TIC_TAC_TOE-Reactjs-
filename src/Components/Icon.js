68f7e368b7b85924139ac3e77e67236b4507ef81
rishab1653 <rishab959290@gmail.com>
2020-10-23 18:22:32 +0530

    Added icon component

---
+++ src/Components/Icon.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {FaTimes , FaPen , FaRegCircle} from 'react-icons/fa'
+
+const Icon = ({name}) => {
+    return (
+        <h1>
+            <FaTimes className="icon"/>
+
+        </h1>
+    )
+}
+
+export default Icon
