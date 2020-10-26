01d015cc8080cdffdba6f6690841ee9d506940ed
rishab1653 <rishab959290@gmail.com>
2020-10-26 11:03:54 +0530

    Added winning logic

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import './App.css';
 const itemArray = new Array(9).fill("empty");
 const App = () => {
   const [isCross , setIsCross] = useState(false);
-  const [winMessage , setWinMessage] =useState("");
+  const [winMessage , setWinMessage] =useState("empty");
 
   const reLoad = () => {
     setIsCross(false)
@@ -19,6 +19,25 @@ const App = () => {
   }
 
   const isWinner = () => {
+
+    if(itemArray[0]===itemArray[1] && itemArray[0]===itemArray[2] && itemArray[0]!="empty"){
+      setWinMessage(`${itemArray[0]} wins`)
+    }
+    else if(itemArray[3]===itemArray[4] && itemArray[3]===itemArray[5] && itemArray[3]!="empty"){
+      setWinMessage(`${itemArray[3]} wins`)}
+    else if(itemArray[6]===itemArray[7] && itemArray[6]===itemArray[8] && itemArray[6]!="empty"){
+        setWinMessage(`${itemArray[6]} wins`)}
+        else if(itemArray[0]===itemArray[3] && itemArray[0]===itemArray[6] && itemArray[0]!="empty"){
+          setWinMessage(`${itemArray[0]} wins`)}
+        else if(itemArray[1]===itemArray[4] && itemArray[1]===itemArray[7] && itemArray[1]!="empty"){
+            setWinMessage(`${itemArray[1]} wins`)}
+            else if(itemArray[2]===itemArray[5] && itemArray[2]===itemArray[8] && itemArray[2]!="empty"){
+              setWinMessage(`${itemArray[2]} wins`)}
+              else if(itemArray[0]===itemArray[4] && itemArray[0]===itemArray[8] && itemArray[0]!="empty"){
+                setWinMessage(`${itemArray[0]} wins`)}
+              else if(itemArray[2]===itemArray[4] && itemArray[2]===itemArray[6] && itemArray[2]!="empty"){
+                  setWinMessage(`${itemArray[2]} wins`)}
+    
     //
   }
 
@@ -44,9 +63,22 @@ const App = () => {
     <ToastContainer position="bottom-center"/>
     <Row>
       <Col md={6} className="offset-md-3">
+      {winMessage ? (
+        <div className="mb-2 mt-2">
+        <h1 className="text-success text-uppercase text-center">
+        {winMessage}
+        </h1>
+        <Button color="success" block onClick={reLoad}>
+        Reload !
+        </Button>
+        </div>
+      ):(<h1 className="text-center text-warning ">
+      {isCross? "Cross":"Cicle"} turns 
+        </h1>)}
+     
       <div className="grid">
         {itemArray.map((item , index)=>(
-          <Card>
+          <Card color="warning" onClick={()=>changeItem(index)}>
           <CardBody className="box">
           <Icon name={item}/>
           </CardBody>
